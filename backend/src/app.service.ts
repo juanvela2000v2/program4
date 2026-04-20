f20a793ce471c8ad8b586c3a9fa5e9e8eb4ffa06
Dersen11 <georgebryamramosjaldin@gmail.com>
2026-04-20 17:28:16 -0400

    Primer commit

---
+++ backend/src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World! cambios desde vic';
+  }
+}
+//https://github.com/juanvela2000v2/program4.git
